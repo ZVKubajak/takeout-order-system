4417586116e0e5093d7377dd11285d2ea4fe3c14
VanceGoblin <zvkubajak@gmail.com>
2024-11-05 08:14:51 -0600

    Removed TODO Comments to Fix Some Flake8 Errors

--- order_system.py
+++ order_system.py
@@ -28,20 +28,13 @@ def place_order(menu):
     print("Welcome to the Generic Take Out Restaurant.")
 
     place_order = True
-    # * TODO: Create a continuous while loop so customers can order multiple items
+
     while place_order:
         item_number = 1
 
-        # * TODO: Loop through the menu dictionary, extracting the food category and
-        # the options for each category
         for food_category, options in menu.items():
-
-            # * TODO: Loop through the options for each food category, extracting the
-            # meal and the price
             for meal, price in options.items():
-                # * TODO: Print the menu item number, food category, meal, and price
                 print(f"{item_number}. {meal} {food_category}: ${price}")
-                # * TODO: Update the menu selection number
                 item_number += 1
 
         # Ask customer to input menu item number
@@ -55,28 +48,22 @@ def place_order(menu):
         # Let the customer know if they should type 'n' or 'N' to quit
         keep_ordering = input("Would you like to keep ordering? (N) to quit: ")
 
-        # * TODO: Write a conditional statement that checks if the customer types
-        # 'n' or 'N'
         if keep_ordering.lower() == "n":
 
             # Since the customer decided to stop ordering, thank them for
             # their order
             print("Thank you for your order.")
 
-            # * TODO: Use a list comprehension to create a list called prices_list,
-            # which contains the total prices for each item in the order list:
-            # The total price for each item should multiply the price by quantity
+            # Total price for each item should multiply the price by quantity
             prices_list = [item["Price"] * item["Quantity"] for item in order]
 
-            # * TODO: Create an order_total from the prices list using sum()
-            # and round the prices to 2 decimal places.
+            # Round the prices to 2 decimal places.
             order_total = round(sum(prices_list), 2)
 
             # Write a break statement or set the condition to False to exit
             # the ordering loop
             place_order = False
 
-    # * TODO: Return the order list and the order total
     return order, order_total
 
 
@@ -95,35 +82,22 @@ def update_order(order, menu_selection, menu_items):
     order (list): A list of dictionaries containing the menu item name, price,
                     and quantity ordered (updated as needed).
     """
-    # * TODO: Check if the customer's input string can be converted
-    # to an integer and prints an error message if it does not
-    if menu_selection.isdigit():
 
-        # * TODO: Convert the menu selection to an integer
+    if menu_selection.isdigit():
         menu_selection = int(menu_selection)
 
-        #  * TODO: Write a conditional statement that checks if the customer's input is
-        # an item on the menu and prints an error message if it is not
         if menu_selection in menu_items:
-
             # Store the item name as a variable
             item_name = menu_items[menu_selection]["Item name"]
 
-            # * TODO: A prompt (input) to the customer that prints the name of the
-            # menu item to the user and asks the quantity they would like to order.
             # Store the return in a quantity variable
             quantity_selection = input(f"How many orders of {item_name} would you like to order? ")
 
-            # * TODO: Write a conditional statement that checks if the input quantity
-            # can be converted to an integer, then converts it to an integer.
-            # Have it default to 1 if it does not.
             if quantity_selection.isdigit():
                 quantity_selection = int(quantity_selection)
             else:
                 quantity_selection = 1
 
-            # * TODO: Add a dictionary with the item name, price, and quantity to the
-            # order list. Use the following names for the dictionary keys:
             # "Item name", "Price", "Quantity"
             entry = {
                 "Item name": item_name,
@@ -138,7 +112,6 @@ def update_order(order, menu_selection, menu_items):
     else:
         print("Error: Please enter a valid menu number.")
 
-    # * TODO: Return the updated order
     return order
 
 
@@ -150,19 +123,12 @@ def print_itemized_receipt(receipt):  # receipt is order
     receipt (list): A list of dictionaries containing the menu item name, price,
                     and quantity ordered.
     """
-    # Uncomment the following line if you need to check the structure of the receipt
-    #print(receipt)
 
-    # * TODO: Loop through the items in the customer's receipt
     for entry in receipt:
-
-        # * TODO: Store the dictionary items ("Item name", "Price", "Quantity") as variables
         item_name = entry["Item name"]
         price = entry["Price"]
         quantity = entry["Quantity"]
 
-        # * TODO: Print the receipt line using the print_receipt_line function
-        # send the item name, price, and quantity as separate arguments
         print_receipt_line(item_name, price, quantity)
 
 
